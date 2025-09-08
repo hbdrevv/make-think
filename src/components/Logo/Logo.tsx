bce6998341fe17b1497c01e8afcb150ecc6239a1
hbdrevv <vanderveendrew@gmail.com>
2025-09-08 09:04:53 -0400

    adjust logo aspect ratio on small devices

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -23,7 +23,7 @@ export const Logo = (props: Props) => {
       loading={loading}
       fetchPriority={priority}
       decoding="async"
-      className={clsx('max-w-[9.375rem] w-full h-[34px]', className)}
+      className={clsx('max-w-[9.375rem] w-full aspect-[193/34] object-contain', className)}
       src={getPublicMediaURLFromResource({ filename: 'drevv_logo_rounded_2x.png' })}
     />
   )
