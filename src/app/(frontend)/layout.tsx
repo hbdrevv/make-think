6d2005954ed9a64d1864bd5bbd93c60e2b061448
hbdrevv <vanderveendrew@gmail.com>
2025-09-09 11:24:53 -0400

    analytics added

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/next'
 import type { Metadata } from 'next'
 
 import { cn } from '@/utilities/ui'
@@ -38,6 +39,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
           {children}
           <Footer />
         </Providers>
+        <Analytics />
       </body>
     </html>
   )
